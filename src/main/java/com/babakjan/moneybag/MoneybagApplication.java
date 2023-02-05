f58aa7cd0cb1b0facee40ab2d5ec0dbb8785d762
Jan Babák <babakhonza@gmail.com>
2023-02-05 14:53:15 +0100

    Add: generate project

---
+++ src/main/java/com/babakjan/moneybag/MoneybagApplication.java
@@ -0,0 +1,13 @@
+package com.babakjan.moneybag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneybagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babakjan/moneybag/MoneybagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babakjan.moneybag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneybagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
